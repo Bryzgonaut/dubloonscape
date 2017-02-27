d114d24d2b33e257dd5ec4225a69ef0c0efcb8da
Dick Van Deke <laserpanda@gmail.com>
2017-02-27 16:23:30 -0600

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public int BaseValue { get; set; }
+        public int Rarity { get; set; }
+        public bool IsEquipable{ get; set; }
+        public condition Condition { get; set; }
+
+        public int TrueValue
+        {
+            get { return BaseValue * Rarity * (int)Condition; }
+        }
+
+    public enum condition
+        {
+            broken = 0,
+            damaged = 1,
+            ordinary = 2,
+            exquisite =3
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item testItem = new Item();
+            testItem.Name = "Test Item 1";
+            testItem.BaseValue = 100;
+            testItem.Rarity = 0;
+            testItem.Condition = Item.condition.broken;
+
+            Console.WriteLine(testItem.Name + ": $" + testItem.TrueValue);
+
+        }
+    }
+}
